daa8757ea3fca9022ba5fd6716fbb2918f7a50a9
Iain D Broadfoot <ibroadfo@gmail.com>
2016-03-20 17:34:50 +0000

    grab baseplugin skeleton

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 /* jshint node: true */
 'use strict';
+var BasePlugin = require('ember-cli-deploy-plugin');
 
 module.exports = {
-  name: 'ember-cli-deploy-firebase'
+  name: 'ember-cli-deploy-firebase',
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name,
+
+      didBuild: function(context) {
+        //do something amazing here once the project has been built
+      },
+
+      upload: function(context) {
+        //do something here to actually deploy your app somewhere
+      },
+
+      didDeploy: function(context) {
+        //do something here like notify your team on slack
+      }
+    });
+
+    return new DeployPlugin();
+  }
 };
